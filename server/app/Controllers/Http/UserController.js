38031a530b8ed3bf7adf41a3d0cad9fa4af5172f
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-11-24 17:41:00 +0200

    register method is changed

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -12,14 +12,13 @@ class UserController {
 
     async register({ request }) {
         const { email, password } = request.all();
-        console.log(email, password);
-        const user = await User.create({
+        await User.create({
             email,
             password,
             username: email,
         });
 
-        return user
+        return this.login(...arguments);
     }
 }
 
