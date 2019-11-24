6965371b8409f1a4503168424717bf0d5ef623a6
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-11-24 18:48:32 +0200

    return method of ProjectController/index() is changed

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -4,9 +4,7 @@ class ProjectController {
     async index({ auth }) {
         const user = await auth.getUser();
         console.log(user.id);
-        return {
-            message: 'Hello World'
-        }
+        return await user.projects().fetch();
     }
 }
 
